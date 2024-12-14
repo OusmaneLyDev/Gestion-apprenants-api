24cc8ab1c3d9423b79a086590aeb06749ccd02db
Ehya-Ag <ehyaagmohamed23@gmail.com>
2024-12-14 11:52:08 +0000

    Adding registration

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import studentRoute from './studentRoute.js'
 import moduleRoute from './moduleRoute.js'
+import registrationRoute from './registrationRoute.js'
 const router = express.Router();
 
 
 
 router.use(studentRoute);
-router.use(moduleRoute)
+router.use(moduleRoute);
+router.use(registrationRoute);
 export default router;
